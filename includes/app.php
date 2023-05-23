92a33df31428296993b782a904aa143a490165d2
felixm <feapta@gmail.com>
2023-05-23 17:59:29 +0200

    otra prueba

--- includes/app.php
+++ includes/app.php
@@ -1,6 +1,7 @@
 
 
 <?php 
+
 require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
@@ -8,7 +9,7 @@ $dotenv->safeLoad();
 require 'funciones.php';
 require 'database.php';
 
-
-// Conectarnos a la base de datos
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
+
+?>
