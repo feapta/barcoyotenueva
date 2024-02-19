ad295772e1d1bde3f0c00620c371d3f515a5c5b7
felixm <feapta@gmail.com>
2024-02-19 09:51:07 +0100

    fallo

--- views/cartas/carta_ofertas.php
+++ views/cartas/carta_ofertas.php
@@ -3,7 +3,7 @@
 <!-- Ofertas de temporada -->
 
 <div class="contenedor contenido_centrado carta_ofertas">
-    <h3> Carta de ofertas</h3>
+    <h3> Bebidas</h3>
 
     <p>Aqui les presentamos las mejores ofertas.</p>
 
