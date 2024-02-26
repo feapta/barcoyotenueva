5b1fcd5b37de664629870f86c60759b381656fbc
felixm <feapta@gmail.com>
2024-02-26 19:49:20 +0100

    ofertas

--- views/ofertas/ofertas.php
+++ views/ofertas/ofertas.php
@@ -17,25 +17,7 @@
                     <img src="/imagenes_ofertas/<?php echo $oferta->imagen; ?>" alt="Imagenes">
                     <h4>    <?php echo $oferta->titulo; ?> </h4>
                     <p>     <?php echo $oferta->descripcion; ?></p>
-                    <h5>    <?php echo $oferta->precio; ?> €</h5>
-                    <p> Valida hasta:&nbsp;
-                        <?php 
-                            $array_dia_semana = array(
-                                1=>"Lunes", 
-                                2=>"Martes", 
-                                3=>"Miércoles", 
-                                4=>"Jueves", 
-                                5=>"Viernes", 
-                                6=>"Sábado",
-                                7=>"Domingo" 
-                            );
-                            $fecha = date_create($oferta->valida);
-                            $Unixfecha = date_timestamp_get($fecha);
-                            $dia_semana = date('N', $Unixfecha);
-                            echo $array_dia_semana[$dia_semana] . " - " .date( "d - m - Y", $Unixfecha);
-
-                        ?>
-                    </p>
+                    <h5>    <?php echo $oferta->precio; ?> €</h5>  
                 </li>
             <?php } ?>
         </ul>
@@ -65,3 +47,25 @@
 
 </div>
 
+<!-- 
+ <p> Valida hasta:&nbsp;
+        <?php 
+            // $array_dia_semana = array(
+            //     1=>"Lunes", 
+            //     2=>"Martes", 
+            //     3=>"Miércoles", 
+            //     4=>"Jueves", 
+            //     5=>"Viernes", 
+            //     6=>"Sábado",
+            //     7=>"Domingo" 
+            // );
+            // $fecha = date_create($oferta->valida);
+            // $Unixfecha = date_timestamp_get($fecha);
+            // $dia_semana = date('N', $Unixfecha);
+            // echo $array_dia_semana[$dia_semana] . " - " .date( "d - m - Y", $Unixfecha);
+
+        ?>
+        
+</p>
+
+-->
